b2e7107d7025fc59a67536e1bf55e495b6a69d5e
sufianfuad <sufianfuad2611@gmail.com>
2021-12-30 21:12:19 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+//express
+const app = express();
+//cors
+const cors = require('cors');
+//
+const { MongoClient } = require('mongodb');
+//dotenv
+require('dotenv').config();
+
+const port = process.env.PORT || 7000;
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+//==========
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i3fcr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
+async function run() {
+    try {
+
+    }
+    finally {
+        // await client.close();   
+    }
+}
+run.catch(console.dir);
+//==============
+
+app.get('/', (req, res) => {
+    res.send('Database Connected successfully');
+})
+
+app.listen(port, () => {
+    console.log(`Running at ${port}`);
+})
